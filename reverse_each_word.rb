186fb0cf7b11dded6dd5b577a9e34a8bd4387f17
Dan Hintz <dhintz@live.com>
2018-08-26 22:44:17 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,13 @@
-def reverse_each_word(sentence)
-  array = sentence.split(" ")
-  new_array = []
-  array.each {|word| new_array << word.reverse}
-  return new_array.join(" ")
-end
-
-#def reverse_each_word_2(sentence)
+#def reverse_each_word(sentence)
 #  array = sentence.split(" ")
-#  array.map {|word| word.reverse}
-#  return array.join(" ")
+#  new_array = []
+#  array.each {|word| new_array << word.reverse}
+#  return new_array.join(" ")
 #end
+
+def reverse_each_word_2(sentence)
+  array = sentence.split(" ")
+  array.map {|word| word.reverse}
+  return array.join(" ")
+end
   
